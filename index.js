7f8dd19197bc662fae017ff2a2a96ff9d274b2e1
navdeep <rajvir.k@iapptechnologies.com>
2023-07-26 18:14:06 +0530

    openAI integration

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 // middilewere
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent, Authorization"
